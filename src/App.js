49b7d200394d109823d2665f62bcc5dc0683dbe7
LauNM <laurie.beduneau@gmail.com>
2022-11-09 17:01:10 +0100

    create react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Error from './pages/error';
+import EmployeeList from './pages/employeeList';
+import CreateEmployee from './pages/createEmployee';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={ <CreateEmployee /> }/>
+        <Route path="/login" element={ <EmployeeList /> }/>
+        <Route path="*" element={ <Error /> } />
+      </Routes>
     </div>
   );
 }
