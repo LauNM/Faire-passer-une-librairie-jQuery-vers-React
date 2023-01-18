e33dfca2caa0a1ab42ae8ede15e6861c51c5a108
LauNM <laurie.beduneau@gmail.com>
2023-01-18 14:51:07 +0100

    removed useless test

--- cypress/e2e/employeeList.cy.js
+++ cypress/e2e/employeeList.cy.js
@@ -20,39 +20,6 @@ describe('Employee List Page with data', () => {
     localStorage.setItem("EmployeeList", JSON.stringify(data));
   })
 
-  it('should have data in localStorage', () => {
-    cy.getAllLocalStorage().should(() => {
-      expect(localStorage.getItem('EmployeeList')).to.deep.equal(JSON.stringify(
-        [
-          {
-            "first-name":"Anthony",
-            "last-name":"Brel",
-            "date-of-birth":"1989-06-11",
-            "start-date":"2023-01-22",
-            "street":"Rue du state",
-            "city":"New york",
-            "state":"NY",
-            "zip-code":"111111",
-            "department":"Sales",
-            "id":"2f75fea1-62e8-4225-b8cd-3a1a6ff6bb0b"
-          },
-          {
-            "first-name":"Charline",
-            "last-name":"Brel",
-            "date-of-birth":"1977-06-30",
-            "start-date":"2020-01-13",
-            "street":"Rue du Stade",
-            "city":"New york",
-            "state":"NY",
-            "zip-code":"111111",
-            "department":"Marketing",
-            "id":"2f75fea1-62e8-4665-b8cd-3a1a6ff6bb0b"
-          }
-        ]
-      ))
-    })
-  })
-
   it('should display same data as localStorage', () => {
     cy.get('.MuiDataGrid-row').should('have.length', 2)
     cy.get('.MuiDataGrid-row').first().contains('Anthony')
