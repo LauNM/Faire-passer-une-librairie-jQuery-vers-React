351640c3c0463c11ea6a157dec470e2e4356b787
LauNM <laurie.beduneau@gmail.com>
2023-01-25 13:26:03 +0100

    run eslint

--- cypress/e2e/createEmployee.cy.js
+++ cypress/e2e/createEmployee.cy.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 describe('Create employee Page', () => {
   beforeEach(() => {
     cy.visit('/');
--- cypress/e2e/employeeList.cy.js
+++ cypress/e2e/employeeList.cy.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const data = require('../fixtures/employeeList.json')
 
 describe('Employee List Page with no data', () => {
